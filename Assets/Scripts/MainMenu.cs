44e264515d00e692b7beea25df3499ab34dbffaa
JayPlayer369 <113185518+JayPlayer369@users.noreply.github.com>
2025-09-30 14:05:03 -0400

    made the damn main menu work

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject mainMenu;
+    public Button startButton;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startButton.onClick.AddListener(Play);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Play()
+    {
+        mainMenu.SetActive(false);
     }
 }
